62269a8496a16ed4d145eaef39a88c8d51299f24
roccovp3 <rocco.perciavalleiii@gmail.com>
2024-08-26 19:00:27 -0500

    can parse lines into tokens robustly

--- Makefile
+++ Makefile
@@ -4,11 +4,12 @@ yash.o: yash.c
 	gcc -c yash.c -o yash.o
 
 format: yash.o 
-	$(shell clang-format -i *.c)
-	gcc yash.o -o yash
+# must have clang-format installed
+	$(shell clang-format -i --style=Microsoft *.c)
+	gcc yash.o -lreadline -o yash
 
 yash: yash.o
-	gcc yash.o -o yash
+	gcc yash.o -lreadline -o yash
 
 clean:
 	-rm -f yash.o
--- yash.c
+++ yash.c
@@ -1,8 +1,61 @@
+#include <readline/history.h>
+#include <readline/readline.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char **argv) {
-  uint32_t x = 10;
-  printf("Hello %d\n", x);
-  return 0;
+#define DEBUG
+
+#define MAX_INPUT_SIZE 2000
+#define MAX_TOKEN_SIZE 30
+
+int parse_user_input(char *user_str)
+{
+    char *token;
+    char tokens[MAX_INPUT_SIZE][MAX_TOKEN_SIZE] = {0};
+
+    token = strtok(user_str, " ");
+
+    for (int i = 0; token != 0; i++)
+    {
+        strncpy(tokens[i], token, MAX_TOKEN_SIZE);
+        token = strtok(0, " ");
+    }
+
+#ifdef DEBUG
+    for (int j = 0; j < MAX_INPUT_SIZE; j++)
+    {
+        if (strcmp(tokens[j], ""))
+            printf("%s\n", tokens[j]);
+    }
+#endif
+
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    // printf("%s", getenv("PATH"));
+    // char *args[] = {"hello\n"};
+    // execvp("printf", args);
+
+    char *user_str;
+    for (;;)
+    {
+        user_str = readline("# ");
+
+        if (strlen(user_str) > MAX_INPUT_SIZE)
+        {
+            printf("Input character limit of %d exceeded. Try again.\n", MAX_INPUT_SIZE);
+        }
+        else
+        {
+            parse_user_input(user_str);
+        }
+
+        free(user_str);
+    }
+
+    return 0;
 }
